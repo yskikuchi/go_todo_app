19acccdf351bf0d92a68f5c5d9caaa237d8149a1
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-22 13:21:08 +0900

    feat: sqlxを使用してデータをDBに保存する処理を実装

--- go.mod
+++ go.mod
@@ -7,12 +7,16 @@ require golang.org/x/sync v0.9.0
 require github.com/caarlos0/env/v6 v6.10.1
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.23.0
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/go-cmp v0.6.0
+	github.com/jmoiron/sqlx v1.4.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- store/task.go
+++ store/task.go
@@ -1,33 +1,39 @@
 package store
 
 import (
-	"errors"
+	"context"
 
 	"github.com/yskikuchi/go_todo_app/entity"
 )
 
-var (
-	Tasks       = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
-	ErrNotFound = errors.New("not found")
-)
-
-type TaskStore struct {
-	LastID entity.TaskID
-	Tasks  map[entity.TaskID]*entity.Task
-}
-
-func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
-	ts.LastID++
-	t.ID = ts.LastID
-	ts.Tasks[t.ID] = t
-	return t.ID, nil
-}
-
-func (ts *TaskStore) All() entity.Tasks {
-	tasks := make([]*entity.Task, len(ts.Tasks))
-	for i, t := range ts.Tasks {
-		tasks[i-1] = t
+func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) error {
+	t.Created = r.Clocker.Now()
+	t.Modified = r.Clocker.Now()
+	sql := `INSERT INTO task
+			(title, status, created, modified)
+		VALUES (?, ?, ?, ?)`
+	result, err := db.ExecContext(ctx, sql, t.Title, t.Status, t.Created, t.Modified)
+	if err != nil {
+		return err
 	}
 
-	return tasks
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	t.ID = entity.TaskID(id)
+	return nil
+}
+
+func (r *Repository) ListTasks(ctx context.Context, db Queryer) (entity.Tasks, error) {
+	tasks := entity.Tasks{}
+	sql := `SELECT
+			id, title,
+			status, created, modified
+			FROM task;
+	`
+	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
+		return nil, err
+	}
+	return tasks, nil
 }
