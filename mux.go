7b2c79ddfab25a5c24870f8a430b379edd37861a
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-28 14:58:33 +0900

    feat: login処理を実装

--- mux.go
+++ mux.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/yskikuchi/go_todo_app/auth"
 	"github.com/yskikuchi/go_todo_app/clock"
 	"github.com/yskikuchi/go_todo_app/config"
 	"github.com/yskikuchi/go_todo_app/handler"
@@ -26,7 +27,26 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	if err != nil {
 		return nil, cleanup, err
 	}
-	r := store.Repository{Clocker: clock.RealClocker{}}
+	clocker := clock.RealClocker{}
+	r := store.Repository{Clocker: clocker}
+	rcli, err := store.NewKVS(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+	jwter, err := auth.NewJWTer(rcli, clocker)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	l := &handler.Login{
+		Service: &service.Login{
+			DB:             db,
+			Repo:           &r,
+			TokenGenerator: jwter,
+		},
+		Validator: v,
+	}
+	mux.Post("/login", l.ServeHTTP)
 	at := &handler.AddTask{
 		Service:   &service.AddTask{DB: db, Repo: &r},
 		Validator: v,
